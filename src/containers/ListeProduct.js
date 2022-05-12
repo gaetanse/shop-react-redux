5f8e1c30c397626db102628da48299f6468a1dbf
gaetan <gs99@hotmail.fr>
2022-05-12 16:38:50 +0200

    design

--- src/containers/ListeProduct.js
+++ src/containers/ListeProduct.js
@@ -2,6 +2,7 @@ import { AddProduct } from "./../redux/actions/ActionProduct"
 import { getArticles } from "./../services/ArticleService"
 import { useEffect } from "react"
 import { useSelector,useDispatch } from "react-redux"
+import { Card,Row,Col } from "react-bootstrap"
 
 export default function ListeProduct() {
 
@@ -26,20 +27,25 @@ export default function ListeProduct() {
     return (
         <div>
 
+<Row xs={1} md={4} className="g-2">
             {
                 listeProducts.lenght !== 0 ?
                 listeProducts.map((e,i)=>{
                     return(
-                        <div>
-                            <p>{e.title}</p>
-                            <img src={e.image}/>
-                            <p>{e.price}</p>
-                        </div>
+                        <Col>
+                        <Card style={{ width: 'auto', height: '300px',border: "solid 1px black" }}>
+                                  <Card.Img variant="top" src={e.image} style={{ width: 'auto', height: '150px',background: "#F7F7F7" }} />
+                            <Card.Body style={{color:"#E42F32"}}>
+                                <Card.Text><strong>{e.title}</strong></Card.Text>
+                                <Card.Text style={{fontSize: "12px"}}><strong>{e.price}</strong></Card.Text>
+                            </Card.Body>
+                        </Card>
+                        </Col>
                     )
                 })
                 : <div></div>
             }
-
+</Row>
         </div>
     )
 }
